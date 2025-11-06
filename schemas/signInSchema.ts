fadd7dcc46dbd28f21eac30e9cfeb7aa531f4b7e
DikshantBadawadagi <dikshantbadawadagi@gmail.com>
2025-11-07 00:23:01 +0530

    Added zod validation

---
+++ schemas/signInSchema.ts
@@ -0,0 +1,13 @@
+import * as z from 'zod';
+
+export const singInSchema = z
+    .object({
+        identifier: z
+            .string()
+            .min(1, {message:"Email is required"})
+            .email({message:"Invalid email address"}),
+        password: z
+            .string()
+            .min(1,{message: "Password is required"})
+            .min(8, {message:"Password must be at least 8 characters long"}),
+    })
